d91db263b88491eb6d26a22cfa84d29b79bbbb53
Carlos Gutiérrez <elimperiodeloefimero@gmail.com>
2020-09-08 18:04:04 -0500

    Se renueva API_KEY y se valida useEffect

--- noticias_react/src/App.js
+++ noticias_react/src/App.js
@@ -1,41 +1,38 @@
-import React, { Fragment, useState, useEffect } from 'react';
-import Header from './components/Header';
-import Formulario from './components/Formulario';
-import ListadoNoticias from './components/ListadoNoticias';
+import React, { Fragment, useState, useEffect } from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+import ListadoNoticias from "./components/ListadoNoticias";
+import { API_KEY } from "./config";
 
 function App() {
-
   // definir la categoria y noticias
-  const [categoria, guardarCategoria] = useState('');
+  const [categoria, guardarCategoria] = useState("");
   const [noticias, guardarNoticias] = useState([]);
 
   useEffect(() => {
     const consultarAPI = async () => {
-      const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=6c1c1dfcb4a943c7bf481bc628b80153`;
+      if (categoria !== "") {
+        const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=${API_KEY}`;
 
-      const respuesta = await fetch(url);
-      const noticias = await respuesta.json();
-
-      guardarNoticias(noticias.articles);
-    }
+        const respuesta = await fetch(url);
+        const noticias = await respuesta.json();
+        guardarNoticias(noticias.articles);
+      } else {
+        return null;
+      }
+    };
     consultarAPI();
   }, [categoria]);
 
   return (
     <Fragment>
-        <Header 
-          titulo='Buscador de Noticias'
-        />
+      <Header titulo="Buscador de Noticias" />
 
-        <div className="container white">
-            <Formulario 
-              guardarCategoria={guardarCategoria}
-            />
+      <div className="container white">
+        <Formulario guardarCategoria={guardarCategoria} />
 
-            <ListadoNoticias 
-              noticias={noticias}
-            />
-        </div>
+        <ListadoNoticias noticias={noticias} />
+      </div>
     </Fragment>
   );
 }
---
+++ noticias_react/src/config.js
@@ -0,0 +1,1 @@
+export const API_KEY = "da6af0c350b449d391b4ecd3e3250759";
