949ba8b1e7120621d88ac505afe1c0aad1393c50
Carlos Gutiérrez <elimperiodeloefimero@gmail.com>
2020-09-11 17:24:59 -0500

    Se agrega a la gestion del estado de la lista de proyectos a react contect para consumirlo en listProyects

--- merntask-cliente/src/components/proyectos/ListadoProyectos.js
+++ merntask-cliente/src/components/proyectos/ListadoProyectos.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import Proyecto from './Proyecto';
+import React, { useContext, useEffect } from "react";
+import Proyecto from "./Proyecto";
+import proyectoContext from "../../context/proyectos/proyectoContext";
 
 const ListadoProyectos = () => {
+  // Extrar proyectos de state inicial
+  const proyectosContext = useContext(proyectoContext);
+  const { proyectos, obtenerProyectos } = proyectosContext;
 
-    const proyectos = [
-        {nombre: 'Tienda Virtual'},
-        {nombre: 'Intranet'},
-        {nombre: 'Diseño de Sitio web'}
-    ]
+  // Obtener proyectos cuando carga el componente
+  useEffect(() => {
+    obtenerProyectos();
+  }, []);
 
-    return ( 
+  // revisar si proyectos tiene contenido
+  if (proyectos.length === 0) return null;
 
-        <ul className="listado-proyectos">
-            {proyectos.map(proyecto => (
-                <Proyecto 
-                    proyecto={proyecto}
-                />
-            ))}
-        </ul>
-     );
-}
- 
+  return (
+    <ul className="listado-proyectos">
+      {proyectos.map((proyecto) => (
+        <Proyecto key={proyecto.id} proyecto={proyecto} />
+      ))}
+    </ul>
+  );
+};
+
 export default ListadoProyectos;
