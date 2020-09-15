a31ded50e3c7644713fd87b8194c1b7f7bcbd3a5
Carlos Gutiérrez <elimperiodeloefimero@gmail.com>
2020-09-14 19:03:42 -0500

    Se agrega proyecto via context

--- merntask-cliente/src/context/proyectos/proyectoState.js
+++ merntask-cliente/src/context/proyectos/proyectoState.js
@@ -1,52 +1,75 @@
-import React, { useReducer } from "react";
+import React, { useReducer } from 'react';
 
-import proyectoContext from "./proyectoContext";
-import proyectoReducer from "./proyectoReducer";
-import { FORMULARIO_PROYECTO, OBTENER_PROYECTOS } from "../../types";
+import uuid from 'uuid';
 
-const ProyectoState = (props) => {
-  const proyectos = [
-    { id: 1, nombre: "Tienda Virtual" },
-    { id: 2, nombre: "Intranet" },
-    { id: 3, nombre: "Diseño de Sitio web" },
-    { id: 4, nombre: "MERN " },
-  ];
+import proyectoContext from './proyectoContext';
+import proyectoReducer from './proyectoReducer';
+import { 
+    FORMULARIO_PROYECTO, 
+    OBTENER_PROYECTOS,
+    AGREGAR_PROYECTO
+} from '../../types';
 
-  const initialState = {
-    proyectos: [],
-    formulario: false,
-  };
 
-  // Dispatch para ejecutar las acciones
-  const [state, dispatch] = useReducer(proyectoReducer, initialState);
 
-  // Serie de funciones para el CRUD
-  const mostrarFormulario = () => {
-    dispatch({
-      type: FORMULARIO_PROYECTO,
-    });
-  };
+const ProyectoState = props => {
 
-  // Obtener los proyectos
-  const obtenerProyectos = () => {
-    dispatch({
-      type: OBTENER_PROYECTOS,
-      payload: proyectos,
-    });
-  };
+    const proyectos = [
+        { id: 1, nombre: 'Tienda Virtual' },
+        { id: 2, nombre: 'Intranet' },
+        { id: 3, nombre: 'Diseño de Sitio web' },
+        { id: 4, nombre: 'MERN '}
+    ]
 
-  return (
-    <proyectoContext.Provider
-      value={{
-        proyectos: state.proyectos,
-        formulario: state.formulario,
-        mostrarFormulario,
-        obtenerProyectos,
-      }}
-    >
-      {props.children}
-    </proyectoContext.Provider>
-  );
-};
+    const initialState = {
+        proyectos : [],
+        formulario : false
+    }
+
+    // Dispatch para ejecutar las acciones
+    const [state, dispatch] = useReducer(proyectoReducer, initialState)
+
+    // Serie de funciones para el CRUD
+    const mostrarFormulario = () => {
+        dispatch({
+            type: FORMULARIO_PROYECTO
+        })
+    }
+
+    // Obtener los proyectos
+    const obtenerProyectos = () => {
+        dispatch({
+            type: OBTENER_PROYECTOS,
+            payload: proyectos
+        })
+    }
+
+    // Agregar nuevo proyecto
+    const agregarProyecto = proyecto => {
+        proyecto.id = uuid.v4();
+
+        // Insertar el proyecto en el state
+        dispatch({
+            type: AGREGAR_PROYECTO,
+            payload: proyecto
+        })
+    }
+
+
+    return (
+        <proyectoContext.Provider
+            value={{
+                proyectos: state.proyectos,
+                formulario: state.formulario,
+                mostrarFormulario,
+                obtenerProyectos,
+                agregarProyecto
+            }}
+        >
+            {props.children}
+        </proyectoContext.Provider>
+        
+    )
+}
 
 export default ProyectoState;
