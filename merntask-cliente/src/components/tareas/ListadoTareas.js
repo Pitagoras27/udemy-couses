2d85a9191c1cb9b33f7b49cc41b05be15137a066
Carlos Gutiérrez <elimperiodeloefimero@gmail.com>
2020-09-15 12:09:39 -0500

    Se agrega proyecto actual al estado global, se muestra en la UI, y se realizan validaciones necesarias

--- merntask-cliente/src/components/tareas/ListadoTareas.js
+++ merntask-cliente/src/components/tareas/ListadoTareas.js
@@ -1,33 +1,47 @@
-import React, { Fragment } from "react";
-import Tarea from "./Tarea";
+import React, { Fragment, useContext} from 'react';
+import Tarea from './Tarea';
+import proyectoContext from '../../context/proyectos/proyectoContext';
 
 const ListadoTareas = () => {
-  const tareasProyecto = [
-    { nombre: "Elegir Plataforma", estado: true },
-    { nombre: "Elegir Colores", estado: false },
-    { nombre: "Elegir Plataformas de pago", estado: false },
-    { nombre: "Elegir Hosting", estado: true },
-  ];
 
-  return (
-    <Fragment>
-      <h2>Proyecto: Tienda Virtual</h2>
+    // Extrar proyectos de state inicial
+    const proyectosContext = useContext(proyectoContext);
+    const { proyecto } = proyectosContext;
 
-      <ul className="listado-tareas">
-        {tareasProyecto.length === 0 ? (
-          <li className="tarea">
-            <p>No hay tareas</p>
-          </li>
-        ) : (
-          tareasProyecto.map((tarea) => <Tarea tarea={tarea} />)
-        )}
-      </ul>
+    // Si no hay proyecto seleccionado
+    if(!proyecto) return <h2>Selecciona un proyecto</h2>;
 
-      <button type="button" className="btn btn-eliminar">
-        Eliminar Proyecto &times;
-      </button>
-    </Fragment>
-  );
-};
+    // Array destructuring para extraer el proyecto actual
+    const [proyectoActual] =  proyecto;
 
+    const tareasProyecto = [
+        { nombre: 'Elegir Plataforma', estado: true},
+        { nombre: 'Elegir Colores', estado: false},
+        { nombre: 'Elegir Plataformas de pago', estado: false},
+        { nombre: 'Elegir Hosting', estado: true}
+    ];
+
+    return ( 
+        <Fragment>
+            <h2>Proyecto: {proyectoActual.nombre} </h2>
+
+            <ul className="listado-tareas">
+                {tareasProyecto.length === 0 
+                    ? (<li className="tarea"><p>No hay tareas</p></li>) 
+                    : tareasProyecto.map(tarea => (
+                        <Tarea
+                            tarea={tarea}
+                        />
+                    ))
+                }
+            </ul>
+
+            <button     
+                type="button"
+                className="btn btn-eliminar"
+            >Eliminar Proyecto &times;</button>
+        </Fragment>
+     );
+}
+ 
 export default ListadoTareas;
