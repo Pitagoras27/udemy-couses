863971cfadad8e12e6c2fd8ab7e109af2c26ee00
Carlos Gutiérrez <elimperiodeloefimero@gmail.com>
2020-09-15 16:26:06 -0500

    Se agrega context de tareas para trabajar este módulo con un estado global

---
+++ merntask-cliente/src/context/tareas/tareaReducer.js
@@ -0,0 +1,13 @@
+import Tarea from "../../components/tareas/Tarea"
+
+const TareasReducer = (state, action) => {
+
+  switch(action.type) {
+
+    default:
+      return state
+  }
+
+}
+
+export default TareasReducer;
