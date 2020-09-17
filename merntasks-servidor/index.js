4e2eb3c9ea14153e29f38d68217d640c1adf494a
Carlos Gutiérrez <elimperiodeloefimero@gmail.com>
2020-09-17 14:54:10 -0500

    Conectado a base da datos mongo, configuración de variables de entorno con dotenv

--- merntasks-servidor/index.js
+++ merntasks-servidor/index.js
@@ -1,1 +1,11 @@
-console.log('desde index.js')
+const express = require('express');
+const conectarDB = require('./config/db.js');
+
+const app = express();
+conectarDB()
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`conectado en el puertso ${PORT}`)
+});
