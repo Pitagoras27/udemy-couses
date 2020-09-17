1ecab5e9abad53ae06fb01598a00cd9c971e1b2b
Carlos Gutiérrez <elimperiodeloefimero@gmail.com>
2020-09-17 18:11:23 -0500

    Se evita q un usuario agregue el mismo mail a la base de datos

--- merntasks-servidor/controllers/usuarioController.js
+++ merntasks-servidor/controllers/usuarioController.js
@@ -1,3 +1,31 @@
-exports.crearUsuario = (req, res) => {
-  console.log(req.body, '<<---')
+  
+const Usuario = require('../models/Usuario');
+
+exports.crearUsuario = async (req, res) => {
+
+    // extraer email y password
+    const { email, password } = req.body;
+
+
+    try {
+        // Revisar que el usuario registrado sea unico
+        let usuario = await Usuario.findOne({ email });
+
+        if(usuario) {
+            return res.status(400).json({ msg: 'El usuario ya existe' });
+        }
+
+
+        // crea el nuevo usuario
+        usuario = new Usuario(req.body);
+
+        // guardar usuario
+        await usuario.save();
+
+        // Mensaje de confirmación
+        res.json({ msg: 'Usuario creado correctamente' });
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('Hubo un error');
+    }
 }
