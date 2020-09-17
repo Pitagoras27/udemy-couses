ff896a76978da2a88593837ff5401896b8656c40
Carlos Gutiérrez <elimperiodeloefimero@gmail.com>
2020-09-17 17:37:18 -0500

    Se agrega endpoint post para gestionar usuarios en backend

--- merntasks-servidor/index.js
+++ merntasks-servidor/index.js
@@ -6,6 +6,11 @@ conectarDB()
 
 const PORT = process.env.PORT || 4000;
 
+app.use(express.json({ extended: true }))
+
+// Middledwares de rutas
+app.use('/api/usuarios', require('./routes/usuarios'))
+
 app.listen(PORT, () => {
   console.log(`conectado en el puertso ${PORT}`)
 });
