cbcbce7efd994fb9eb6537b517583ff2ab7a6458
Carlos Gutiérrez <elimperiodeloefimero@gmail.com>
2020-09-18 14:06:57 -0500

    Se obtiene lista de proyectos con get

--- merntasks-servidor/controllers/proyectoController.js
+++ merntasks-servidor/controllers/proyectoController.js
@@ -1,6 +1,11 @@
 const Proyecto = require('../models/Proyecto');
+const { validationResult } = require('express-validator');
 
 exports.crearProyecto = async (req, res) => {
+  const errores = validationResult(req);
+  if( !errores.isEmpty() ) {
+      return res.status(400).json({errores: errores.array() })
+  }
   try {
     const proyecto = new Proyecto(req.body);
     proyecto.creador = req.usuario.id;
@@ -9,5 +14,16 @@ exports.crearProyecto = async (req, res) => {
   } catch (error) {
     console.log(error)
     res.status(500).send('Hubo un error');
+  }
+}
+
+// Obtiene todos los proyectos del usuario actual
+exports.obtenerProyectos = async (req, res) => {
+  try {
+      const proyectos = await Proyecto.find({ creador: req.usuario.id }).sort({ creado: -1 });
+      res.json({ proyectos });
+  } catch (error) {
+      console.log(error);
+      res.status(500).send('Hubo un error');
   }
 }
--- merntasks-servidor/routes/proyectos.js
+++ merntasks-servidor/routes/proyectos.js
@@ -1,18 +1,23 @@
 const express = require('express');
-const router = express.Router();
+const { check } = require('express-validator');
 
 const auth = require('../middleware/auth');
+
+const router = express.Router();
 
 const proyectoController = require('../controllers/proyectoController');
 
 router.post('/',
     auth,
+    [
+      check('nombre', 'El nombre del proyecto es obligatorio').not().isEmpty()
+    ],
     proyectoController.crearProyecto
 );
 
 router.get('/',
     auth,
-    proyectoController.crearProyecto,
+    proyectoController.obtenerProyectos,
 );
 
 module.exports = router;
