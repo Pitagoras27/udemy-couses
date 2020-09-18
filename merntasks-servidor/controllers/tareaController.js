0aefca9c2b8f6df95e4a03b44dc636c79854e529
Carlos Gutiérrez <elimperiodeloefimero@gmail.com>
2020-09-18 17:32:24 -0500

    Se agrega router y controlador de tareas

---
+++ merntasks-servidor/controllers/tareaController.js
@@ -0,0 +1,10 @@
+const Tarea = require('../models/Tarea');
+const Proyecto = require('../models/Proyecto');
+const { validationResult } = require('express-validator');
+
+exports.crearTarea = async (req, res) => {
+  const errores = validationResult(req);
+  if( !errores.isEmpty() ) {
+      return res.status(400).json({errores: errores.array() })
+  }
+}
--- merntasks-servidor/index.js
+++ merntasks-servidor/index.js
@@ -12,6 +12,7 @@ app.use(express.json({ extended: true }))
 app.use('/api/usuarios', require('./routes/usuarios'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/proyectos', require('./routes/proyectos'))
+app.use('/api/tareas', require('./routes/tareas'))
 
 app.listen(PORT, () => {
   console.log(`conectado en el puerto ${PORT}`)
