0edf4ce230e08c3ce11aca881bab0785ff7d2416
Carlos Gutiérrez <elimperiodeloefimero@gmail.com>
2020-09-18 14:37:07 -0500

    Se agrega método para actualizar los proyectos

--- merntasks-servidor/routes/proyectos.js
+++ merntasks-servidor/routes/proyectos.js
@@ -20,4 +20,13 @@ router.get('/',
     proyectoController.obtenerProyectos,
 );
 
+// Actualizar proyecto via ID
+router.put('/:id',
+    auth,
+    [
+      check('nombre', 'El nombre del proyecto es obligatoio').not().isEmpty()
+    ],
+    proyectoController.actualizarProyecto
+);
+
 module.exports = router;
