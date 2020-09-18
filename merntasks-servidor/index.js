d0abfbfb322c294ddbeac50ec9cb0a4dab96ed01
Carlos Gutiérrez <elimperiodeloefimero@gmail.com>
2020-09-18 11:58:03 -0500

    implementacion de endpoint api/proyectos almacenamiento en mongo

--- merntasks-servidor/index.js
+++ merntasks-servidor/index.js
@@ -11,6 +11,7 @@ app.use(express.json({ extended: true }))
 // Middledwares de rutas
 app.use('/api/usuarios', require('./routes/usuarios'))
 app.use('/api/auth', require('./routes/auth'))
+app.use('/api/proyectos', require('./routes/proyectos'))
 
 app.listen(PORT, () => {
   console.log(`conectado en el puerto ${PORT}`)
---
+++ merntasks-servidor/routes/proyectos.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const proyectoController = require('../controllers/proyectoController');
+
+router.post('/',
+    proyectoController.crearProyecto
+);
+
+module.exports = router;
